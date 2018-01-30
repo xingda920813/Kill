e28b1f6de12266f10e10c126d684e2d9cf8b25ac
liuhuo.xd <liuhuo.xd@alibaba-inc.com>
2018-01-30 12:45:24 +0800

    Android 8.0 Hack.

--- server/src/main/java/com/xdandroid/server/TargetActivity.java
+++ server/src/main/java/com/xdandroid/server/TargetActivity.java
@@ -1,9 +1,64 @@
 package com.xdandroid.server;
 
-public class TargetActivity extends BaseActivity {
+import android.app.*;
+import android.content.pm.*;
+import android.os.*;
+import android.util.*;
 
+import java.lang.reflect.*;
+import java.util.*;
+
+/**
+ * coreApp="true"
+ * android:sharedUserId="android.uid.system"
+ * android:process="system"
+ * android:theme="@android:style/Theme.NoDisplay"
+ */
+public class TargetActivity extends Activity {
+
+    static Field sAppInfoField;
+
+    static ApplicationInfo mapToAppInfo(Object pkg) {
+        if (sAppInfoField == null)
+            for (Field f : pkg.getClass().getDeclaredFields())
+                if (ApplicationInfo.class.isAssignableFrom(f.getType())) {
+                    f.setAccessible(true);
+                    sAppInfoField = f;
+                    break;
+                }
+        try {
+            return (ApplicationInfo) sAppInfoField.get(pkg);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
-    Object getToken() {
-        return "Target";
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Map<String, Integer> whiteListApps = new HashMap<>();
+            whiteListApps.put("com.tencent.mm", 23);
+            IPackageManager.Stub pms = (IPackageManager.Stub) ServiceManager.getService("package");
+            Field packagesField = pms.getClass().getDeclaredField("mPackages");
+            packagesField.setAccessible(true);
+            ArrayMap<String, ?> packages = (ArrayMap<String, ?>) packagesField.get(pms);
+            packages.values()
+                    .stream()
+                    .filter(Objects::nonNull)
+                    .map(TargetActivity::mapToAppInfo)
+                    .filter(Objects::nonNull)
+                    .forEach(ai -> {
+                        assert ai != null;
+                        if (ai.targetSdkVersion >= Build.VERSION_CODES.M) ai.targetSdkVersion = Build.VERSION_CODES.CUR_DEVELOPMENT - 1;
+                        if (ai.targetSdkVersion <= Build.VERSION_CODES.LOLLIPOP_MR1) ai.targetSdkVersion = Build.VERSION_CODES.LOLLIPOP_MR1;
+                        ai.targetSdkVersion = whiteListApps.getOrDefault(ai.packageName, ai.targetSdkVersion);
+                    });
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        finish();
     }
 }
