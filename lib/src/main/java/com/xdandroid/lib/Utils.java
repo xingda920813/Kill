b9d0b820c89f36cf485837749f7b5d5a6f420a72
Da Xing <570396198@qq.com>
2018-08-19 01:56:39 +0800

    opt.

--- lib/src/main/java/com/xdandroid/lib/Utils.java
+++ lib/src/main/java/com/xdandroid/lib/Utils.java
@@ -73,10 +73,27 @@ public interface Utils {
         }
     }
 
+    static <T> int indexOf(T[] array, T value) {
+        if (array == null) return -1;
+        for (int i = 0; i < array.length; i++) if (Objects.equals(array[i], value)) return i;
+        return -1;
+    }
+
+    static <T> boolean contains(T[] array, T value) {
+        return indexOf(array, value) != -1;
+    }
+
+    static <T> boolean containsAll(T[] array, T[] check) {
+        if (check == null) return true;
+        for (T checkItem : check) if (!contains(array, checkItem)) return false;
+        return true;
+    }
+
     @SuppressWarnings("unchecked")
-    static <T> T[] combineArray(T[] a, T[] b) {
-        T[] ts = (T[]) Array.newInstance(a.getClass().getComponentType(), a.length + b.length);
-        for (int i = 0; i < a.length + b.length; i++) if (i < a.length) ts[i] = a[i]; else ts[i] = b[i - a.length];
+    static <T> T[] combineArray(T[] oriFirst, T[] newLast) {
+        if (containsAll(oriFirst, newLast)) return oriFirst;
+        T[] ts = (T[]) Array.newInstance(oriFirst.getClass().getComponentType(), oriFirst.length + newLast.length);
+        for (int i = 0; i < oriFirst.length + newLast.length; i++) if (i < oriFirst.length) ts[i] = oriFirst[i]; else ts[i] = newLast[i - oriFirst.length];
         return ts;
     }
 
