3472b36880fdefee58bd7dcafb2d94c4912e19df
liuhuo.xd <liuhuo.xd@alibaba-inc.com>
2018-10-09 13:17:23 +0800

    WeChat 保护.

--- server/src/main/java/com/xdandroid/server/Hack.java
+++ server/src/main/java/com/xdandroid/server/Hack.java
@@ -42,6 +42,7 @@ public class Hack implements Utils {
                 .filter(ai -> (ai.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0)
                 .filter(ai -> WHITE_LIST_APP_NAME_SLICES.stream().noneMatch(slice -> ai.packageName.contains(slice)));
         if (targetSdk == 0) aiStream.forEach(ai -> {
+            if ("com.tencent.mm".equals(ai.packageName)) return;
             if (ai.targetSdkVersion >= Build.VERSION_CODES.M) ai.targetSdkVersion = Build.VERSION.SDK_INT == Build.VERSION_CODES.O
                     ? Build.VERSION_CODES.O : Build.VERSION_CODES.CUR_DEVELOPMENT - 1;
             if (ai.targetSdkVersion <= Build.VERSION_CODES.LOLLIPOP_MR1) ai.targetSdkVersion = Build.VERSION_CODES.LOLLIPOP_MR1;
