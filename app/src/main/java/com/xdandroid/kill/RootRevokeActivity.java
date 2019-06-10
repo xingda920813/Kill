2d0f0d46ce92cc15e81a625e4d82b5a45ecd81e5
Da Xing <570396198@qq.com>
2019-06-10 20:45:20 +0800

    Recompile apks.

--- app/src/main/java/com/xdandroid/kill/RootRevokeActivity.java
+++ app/src/main/java/com/xdandroid/kill/RootRevokeActivity.java
@@ -18,30 +18,32 @@ import java.util.*;
  */
 public class RootRevokeActivity extends Activity {
 
+    static int runAsRoot(List<String> revokeOps) throws IOException, InterruptedException {
+        Process proc = null;
+        try {
+            proc = Runtime.getRuntime().exec("su");
+            OutputStream out = proc.getOutputStream();
+            for (String op : revokeOps) {
+                out.write(op.getBytes(StandardCharsets.UTF_8));
+                out.flush();
+            }
+            out.write("exit\n\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            return proc.waitFor();
+        } finally {
+            if (proc != null) proc.destroy();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "RootRevoke", Toast.LENGTH_SHORT).show();
         List<String> revokeOps = GenOpsActivity.getRevokeOps(this, false);
-        Process proc = null;
         try {
-            proc = Runtime.getRuntime().exec("su");
-            OutputStream out = proc.getOutputStream();
-            revokeOps.forEach(op -> {
-                try {
-                    out.write(op.getBytes(StandardCharsets.UTF_8));
-                    out.flush();
-                } catch (IOException e) {
-                    throw Utils.asUnchecked(e);
-                }
-            });
-            out.write("exit\n\n".getBytes(StandardCharsets.UTF_8));
-            out.flush();
-            proc.waitFor();
+            runAsRoot(revokeOps);
         } catch (IOException | InterruptedException e) {
             throw Utils.asUnchecked(e);
-        } finally {
-            if (proc != null) proc.destroy();
         }
         finish();
     }
